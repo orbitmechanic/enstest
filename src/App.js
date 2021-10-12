34dfc54d16a63247c408e307da660ab3327770df
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2021-10-12 17:45:28 -0400

    Experimented.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import ENSAddress from "@ensdomains/react-ens-address";
 import { useMoralis } from "react-moralis";
 import { useEffect } from "react";
 
-function App() {
+function App(props) {
   const { web3, enableWeb3, isWeb3Enabled } = useMoralis();
 
   useEffect(() => {
@@ -10,10 +10,23 @@ function App() {
       enableWeb3();
     }
   }, [isWeb3Enabled, enableWeb3]);
+
   return (
     <div className="App">
       {isWeb3Enabled && (
-        <ENSAddress provider={web3.givenProvider || web3.currentProvider} />
+        <ENSAddress
+          provider={web3.givenProvider || web3.currentProvider}
+          onResolve={({ name, address, type }) => {
+            if (
+              type &&
+              address !== "0x0000000000000000000000000000000000000000"
+            ) {
+              console.log("name:", name);
+              console.log("address:", address);
+              console.log("type:", type);
+            }
+          }}
+        />
       )}
       <p>Orbiter.</p>
     </div>
